e957c1741686f35551c7e4f970d0f560f877c9e2
Arthpatel01 <76816540+Arthpatel01@users.noreply.github.com>
2023-04-14 17:36:38 +0530

    Update wsgi.py

--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
+
+# application = get_wsgi_application()
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
+from whitenoise import WhiteNoise
+from myapp import settings  # Replace 'myapp' with the name of your Django application
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
+application.add_files(settings.STATIC_ROOT)
